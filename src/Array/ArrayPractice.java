986276ea4a83b097fd553ab58d88755dded3d9d5
drarifsun <drarifsun@gmail.com>
2017-12-26 17:47:58 -0500

    Array practice 12/26/2017

---
+++ src/Array/ArrayPractice.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] a = {10,5,7,8,21};
+		
+		for (int i=0;i<4;i++);
+		{
+			System.out.println(a[0]);
+			System.out.println(a[2]);
+			System.out.println(a[4]);
+			System.out.println(a[1]);
+			System.out.println(a[3]);
+		}
+	}
+
+}
