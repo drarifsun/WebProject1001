952dd87201c5942b5fdf7530c14bcc7447fbe90c
drarifsun <drarifsun@gmail.com>
2017-12-28 15:28:23 -0500

    Assignment December 24th, 2017 submission: Weekly appointment

---
+++ src/Home_Assignment_Dec17/Weekly_appointment.java
@@ -0,0 +1,47 @@
+package Home_Assignment_Dec17;
+
+public class Weekly_appointment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("\n");
+		System.out.println("Home Task assigned by Dr. Lutfor Rahman Khan on December 24th, 2017.\n");
+		System.out.println("Topic: Based on day value make a weekday program.");
+		System.out.println("==========================================================\n");
+		
+		
+		int day=7;
+		switch (day)
+		{
+		case 1:
+			System.out.println("Monday: \n \t7:30am: Official board meeting.\n \t2:00pm: Attend lunch at Westin.\n \t4:30: Pick George from school.");
+			break;
+			
+		case 2:
+			System.out.println("Tuesday: \n \t9:00am: Doctor's appointment.\n \t1:30pm: Attending Teacher Parent meeting at school.\n \t7:30pm: Ryan's wedding party.");
+			break;
+			
+		case 3:
+			System.out.println("Wednesday: \n \t9:00am: Interview at Google inc.\n \t4:00pm: Home Owners association meeting.");
+			break;
+			
+		case 4:
+			System.out.println("Thursday: \n \t2:30am: Flight to San Diego.\n \t7:30am: Kayacking with my buddy Rodney.\n \t11:30am: BUsiness meeting with Xenon inc.");
+			break;
+			
+		case 5:
+			System.out.println("Friday: \n \t4:30am: Come back to New York.\n \t12:30pm: Reporting to Boss.");
+			break;
+			
+		case 6:
+			System.out.println("Saturday: \n \t8:00am Playig golf with Marsha.\n \t4:00pm: Surfing at Belmar beach.");
+			break;
+			
+		case 7:
+			System.out.println("9:00am: \n \tPool side party.");
+		}
+	}
+
+}
