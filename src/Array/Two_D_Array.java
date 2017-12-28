eacd23dcc71e30914a530d1c46fdc7d55cd6568e
drarifsun <drarifsun@gmail.com>
2017-12-28 11:43:57 -0500

    another 2D array practice 12/28/2017

--- src/Array/Two_D_Array.java
+++ src/Array/Two_D_Array.java
@@ -9,7 +9,7 @@ public class Two_D_Array {
 		//2D Array declaration
 		
 		
-		int [][] web_grid = new int [4][4];
+		/*int [][] web_grid = new int [4][4];
 		
 		
 		//Data input
@@ -23,6 +23,14 @@ public class Two_D_Array {
 		web_grid[1][1] = 60;
 		web_grid[1][2] = 70;
 		web_grid[1][3] = 80;
+		web_grid[2][0] = 70;
+		web_grid[2][1] = 60;
+		web_grid[2][2] = 50;
+		web_grid[2][3] = 40;
+		web_grid[3][0] = 30;
+		web_grid[3][1] = 20;
+		web_grid[3][2] = 10;
+		web_grid[3][3] = 90;
 		
 		for (int i=0;i<4;i++)
 		{
@@ -33,7 +41,33 @@ public class Two_D_Array {
 			
 				System.out.println();
 		}
+		*/
 		
+		
+		//===================================================================
+		
+		
+		int [][] bytes = new int [3][3];
+		
+		bytes[0][0]= 23;
+		bytes[0][1]= 65;
+		bytes[0][2]= 45;
+		bytes[1][0]= 54;
+		bytes[1][1]= 12;
+		bytes[1][2]= 52;
+		bytes[2][0]= 25;
+		bytes[2][1]= 85;
+		bytes[2][2]= 67;
+	
+		
+		for (int i=0;i<3;i++)
+		{
+			for (int j=0;j<2;j++)
+			{
+				System.out.print(" "+bytes[i][j]);
+			}
+				System.out.println();
+		}
 	}
 
 }
