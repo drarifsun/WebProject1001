3e4d66e2b12ea208f42fdec1f6f4d971b816aa25
drarifsun <drarifsun@gmail.com>
2017-12-28 14:21:17 -0500

    Assignment 'Data type information' commited

---
+++ src/Home_Assignment_Dec17/Data_info_size.java
@@ -0,0 +1,49 @@
+package Home_Assignment_Dec17;
+
+public class Data_info_size {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("\n");
+		System.out.println("Home Task assigned by Dr. Lutfor Rahman Khan on December 24th, 2017.\n");
+		System.out.println("Topic: Based on Data Type make a Data information program.");
+		System.out.println("==========================================================\n");
+		System.out.println("In Java there are two types of data.\n \ta) Primitive and\n \tb) Non-Primitive");
+		
+		String JavaData1;
+		JavaData1 = "Primitive";
+		
+		System.out.println("\nPrimitive data include Boolean and Numeric data type.\n");
+		System.out.println("Numeric data subdivided into:\n \t*Character and Integer type.");
+		System.out.println("\t*Integer again divided into:\n \ta) byte\n \tb) short\n \tc) int\n \td) long\n");
+		System.out.println("Following are "+JavaData1+" data types with their allocated sizes:\n");
+		System.out.print("=======================================================================\n");
+		
+		String[] object = new String[11];
+		
+		object[0] = "boolean= 1 bit " + "value=false";
+		object[1] = "char=    2 bytes " + "value="+"\\u0000";
+		object[2] = "byte=    1 byte " + "value=0";
+		object[3] = "short=   2 bytes " + "value=0";
+		object[4] = "int=     4 bytes " + "value=0";
+		object[5] = "long=    8 bytes " + "value=0L";
+		object[6] = "float=   4 bytes " + "value=0.0f";
+		object[7] = "double=  8 bytes " + "value=0.0d";
+		
+		System.out.println(object[0] + "\n" + object[1] + "\n" + object[2] + "\n" + object[3] + "\n" + object[4] + "\n" + object[5] + "\n" + object[6] + "\n" + object[7] + "\n");
+		
+		
+		String JavaData2;
+		JavaData2 = "Non-Primitive";
+		
+		object[8] = "String";
+		object[9] = "Array";
+		object[10]= "Others";
+		
+		System.out.println("Following are the Non-Primitive data types.");
+		System.out.println("============================================\n");
+		System.out.println(object[8] + "\n" + object[9] + "\n" + object[10]);
+	}
+
+}
---
+++ src/Home_Assignment_Dec17/Result_finder.java
@@ -0,0 +1,84 @@
+package Home_Assignment_Dec17;
+
+public class Result_finder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		System.out.println("Assignment #1 : Based on total marks, make a result finder program");
+		System.out.println("===================================================================\n");
+		System.out.println("using 'if_else if_else' statement");
+		System.out.println("===================================\n");
+		System.out.println("Total marks 1000");
+		System.out.println("First Division= 600 or more");
+		System.out.println("Second Division= 450 or <600");
+		System.out.println("Third Division= 330 or <450\n");
+		
+		String Result;
+		Result = ">=450";
+
+		if (Result==">=600")
+		{
+			System.out.println("Result: The student has passed with a 'First Division'.");
+		}
+
+		else if (Result==">=450")
+		{
+			System.out.println("Result: The student has passed with a 'Second Division'.");
+		}
+
+		else
+		{
+			System.out.println("Result: The student has either passed with a 'Third Division' or failed.");
+		}
+		*/
+		
+		
+		
+		//====================================================================================================
+		
+		
+		
+		System.out.println("Assignment #1 : Based on total marks, make a result finder program");
+		System.out.println("===================================================================\n");
+		System.out.println("using 'Switch' statement");
+		System.out.println("===================================\n");
+		System.out.println("Total marks 1000");
+		System.out.println("First Division= 600 or more");
+		System.out.println("Second Division= 450 or <600");
+		System.out.println("Third Division= 330 or <450\n");
+		
+		
+		int result=1000;
+		
+		switch (result)
+		{
+		case 1000:
+			System.out.println("Full marks = 1000");
+			break;
+			
+		case 600:
+			System.out.println("Result: The student has passed with a 'First Division'.");
+			break;
+			
+		case 450:
+			System.out.println("Result: The student has passed with a 'Second Division'.");
+			break;
+			
+		case 330:
+			System.out.println("Result: The student has passed with a 'Third Division'.");
+			break;
+			
+		case 329:
+			System.out.println("Result: The student has 'Failed'.");
+			break;
+			
+		default:
+			System.out.println("Student not found.");
+			} 
+		
+	
+	}
+
+}
