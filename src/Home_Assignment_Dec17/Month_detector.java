6840198d379782adc7c82d47799a6738cdc1670b
drarifsun <drarifsun@gmail.com>
2017-12-28 15:43:42 -0500

    Assignment December 24th, 2017 submitted: Month detector

---
+++ src/Home_Assignment_Dec17/Month_detector.java
@@ -0,0 +1,66 @@
+package Home_Assignment_Dec17;
+
+public class Month_detector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("\n");
+		System.out.println("Home Task assigned by Dr. Lutfor Rahman Khan on December 24th, 2017.\n");
+		System.out.println("Topic: Based on month value make a month detector program.");
+		System.out.println("==========================================================\n");
+		
+		
+		int month=10;
+		switch (month)
+		{
+		case 1:
+			System.out.println("January");
+			break;
+			
+		case 2:
+			System.out.println("February");
+			break;
+			
+		case 3:
+			System.out.println("March");
+			break;
+			
+		case 4:
+			System.out.println("April");
+			break;
+			
+		case 5:
+			System.out.println("May");
+			break;
+			
+		case 6:
+			System.out.println("June");
+			break;
+			
+		case 7:
+			System.out.println("July");
+			break;
+			
+		case 8:
+			System.out.println("August");
+			break;
+			
+		case 9:
+			System.out.println("September");
+			break;
+			
+		case 10:
+			System.out.println("October");
+			break;
+			
+		case 11:
+			System.out.println("November");
+			break;
+			
+		case 12:
+			System.out.println("December");
+		}
+	}
+
+}
