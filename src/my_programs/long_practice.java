416dd71f115b35660b111ef01ef6543dc24444c0
drarifsun <drarifsun@gmail.com>
2018-01-03 12:27:21 -0500

    Practice 'long' 1st commit

---
+++ src/my_programs/long_practice.java
@@ -0,0 +1,17 @@
+package my_programs;
+
+public class long_practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long hour=5;
+		long speed=3;
+		long distance;
+		distance = hour*3;
+		System.out.print("in "+hour +" hours Josh will run ");
+		System.out.println( distance + " miles.");
+
+	}
+
+}
