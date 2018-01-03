c18bea8ef8e11a26eb4b2e8b347d12caf5a150bb
drarifsun <drarifsun@gmail.com>
2018-01-03 11:43:17 -0500

    my second commit on array practice

--- src/Array/ArrayPractice.java
+++ src/Array/ArrayPractice.java
@@ -8,6 +8,7 @@ public class ArrayPractice {
 		
 		//Array declaration and creation in same line
 		
+		/*
 		int[] a = {10,5,7,8,21};
 		
 		for (int i=0;i<4;i++);
@@ -17,7 +18,20 @@ public class ArrayPractice {
 			System.out.println(a[4]);
 			System.out.println(a[1]);
 			System.out.println(a[3]);
+			
+			*/
+		
+		//============================================================
+		
+		int[] p = {11, 22, 33, 44, 55};
+		
+		for (int j=4; j>0; j--);
+		
+		System.out.println(p[0]);
+		System.out.println(p[1]);
+		System.out.println(p[2]);
+		System.out.println(p[3]);
+		System.out.println(p[4]);
 		}
 	}
 
-}
