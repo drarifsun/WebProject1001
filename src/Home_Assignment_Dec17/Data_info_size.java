d4eee6a658ed606c303b2eb5767101f336694233
drarifsun <drarifsun@gmail.com>
2018-01-03 12:19:43 -0500

    second commit Data info__Blood pressure

--- src/Home_Assignment_Dec17/Data_info_size.java
+++ src/Home_Assignment_Dec17/Data_info_size.java
@@ -5,6 +5,8 @@ public class Data_info_size {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		/*
 		System.out.println("\n");
 		System.out.println("Home Task assigned by Dr. Lutfor Rahman Khan on December 24th, 2017.\n");
 		System.out.println("Topic: Based on Data Type make a Data information program.");
@@ -14,7 +16,7 @@ public class Data_info_size {
 		String JavaData1;
 		JavaData1 = "Primitive";
 		
-		System.out.println("\nPrimitive data include Boolean and Numeric data type.\n");
+		System.out.println("\n"+JavaData1+" data include Boolean and Numeric data type.\n");
 		System.out.println("Numeric data subdivided into:\n \t*Character and Integer type.");
 		System.out.println("\t*Integer again divided into:\n \ta) byte\n \tb) short\n \tc) int\n \td) long\n");
 		System.out.println("Following are "+JavaData1+" data types with their allocated sizes:\n");
@@ -44,6 +46,33 @@ public class Data_info_size {
 		System.out.println("Following are the "+JavaData2+" data types.");
 		System.out.println("=============================================\n");
 		System.out.println(object[8] + "\n" + object[9] + "\n" + object[10]);
+		
+		*/
+		
+		//==========================================================================================================
+		
+		String vitals;
+		vitals="blood pressure";
+		
+		System.out.println("Following are the "+vitals+" of persons' name mentioned:\n");
+		
+		String[] object= new String[10];
+		
+		object[0]= "A. Rahman:    systolic 120 mm Hg "+"diastolic 80 mm Hg" ;
+		object[1]= "Md. Mirza:    systolic 110 mm Hg "+"diastolic 65 mm Hg" ;
+		object[2]= "Tabassum Ara: systolic 120 mm Hg "+"diastolic 70 mm Hg" ;
+		object[3]= "Jharna Paul:  systolic 120 mm Hg "+"diastolic 90 mm Hg" ;
+		object[4]= "Mozammel Haq: systolic 100 mm Hg "+"diastolic 70 mm Hg";
+		object[5]= "Syed Qurban:  systolic 120 mm Hg "+"diastolic 85 mm Hg" ;
+		object[6]= "Md. Kashem:   systolic 110 mm Hg "+"diastolic 80 mm Hg" ;
+		object[7]= "Minoti Rani:  systolic 130 mm Hg "+"diastolic 80 mm Hg" ;
+		object[8]= "A. Shakur:    systolic 125 mm Hg "+"diastolic 85 mm Hg" ;
+		object[9]= "Tribedi saha: systolic 120 mm Hg "+"diastolic 70 mm Hg" ;
+		
+		System.out.println(object[0] +"\n" + object[1]+ "\n" + object[2] +"\n" + object[3] +"\n" + object[4] +"\n" + object[5] +"\n" + object[6] +"\n" + object[7] + "\n" + object[8] + "\n" + object[9] + "\n");
+		System.out.println("\nComment: All are having blood pressure within normal range.");
+		 
+		
 	}
 
 }
